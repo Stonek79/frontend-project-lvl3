7a5c8db9a704d25f5fd050ec850a00c8365da044
Stonek79 <amit-yurist@mail.ru>
2021-01-22 10:23:58 +0300

    Test commit

--- src/getter.js
+++ src/getter.js
@@ -1,9 +1,4 @@
 import axios from 'axios';
 
 export default (url) => axios(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`)
-  .then((response) => {
-    if (!response.data.status.content_type.includes('rss')) {
-      throw new Error('mustHaveRSS');
-    }
-    return response.data;
-  });
+  .then((response) => response.data);
